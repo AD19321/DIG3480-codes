9a4c7478366327d0b62d9f3096d9ae4e6e3553fe
AD19321 <42630488+AD19321@users.noreply.github.com>
2018-09-23 14:46:38 -0400

    Add files via upload

--- Script/rotator.cs
+++ Script/rotator.cs
@@ -6,6 +6,7 @@ public class rotator : MonoBehaviour {
 
 	
 	// Update is called once per frame
+    //rotates the pick ups
 	void Update () {
         transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
 	}
