9bcfc0c0669f9b8193ef6ee7034da036b8547131
AD19321 <42630488+AD19321@users.noreply.github.com>
2018-11-07 12:51:21 -0500

    Add files via upload

---
+++ Space Shooter scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+public class PlayerController : MonoBehaviour {
+    private Rigidbody rgb;
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    private float nextFire;
+    public AudioSource weaponSound;
+    
+    private void Start()
+    {
+        rgb = GetComponent<Rigidbody>();
+        weaponSound = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            weaponSound.Play();
+        }
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rgb.velocity = movement*speed;
+
+        rgb.position = new Vector3
+            (
+                Mathf.Clamp(rgb.position.x,boundary.xMin,boundary.xMax),
+                0.0f,
+                Mathf.Clamp(rgb.position.z,boundary.zMin,boundary.zMax)
+
+            );
+        rgb.rotation = Quaternion.Euler(0.0f, 0.0f, rgb.velocity.x*-tilt);
+    }
+}
---
+++ Space Shooter scripts/WeaponController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour {
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    public float delay;
+
+    private AudioSource source;
+	// Use this for initialization
+	void Start () {
+        source =GetComponent<AudioSource>();
+        InvokeRepeating ("Fire", delay, fireRate);
+	}
+	
+	
+	void Fire () {
+        Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        source.Play();
+	}
+}
